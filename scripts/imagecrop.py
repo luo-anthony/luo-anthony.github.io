ca3e4a703ad6786ebfe43a3e4335013d376bcf26
luo-anthony <anthony.luo@columbia.edu>
2023-01-19 16:35:03 -0500

    update photography page

--- scripts/imagecrop.py
+++ scripts/imagecrop.py
@@ -4,7 +4,7 @@ from os.path import isfile, join
 
 # https://stackoverflow.com/questions/61201141/how-can-i-crop-an-image-with-11-aspect-ratio-using-pillow-in-python
 
-def crop_image(image):
+def crop_image_square(image):
     width, height = image.size
     if width == height:
         return image
@@ -15,18 +15,27 @@ def crop_image(image):
         image = image.crop([0,offset,width,height-offset])
     return image
 
-img_path = "/Users/anthonyluo/Documents/WebsiteFullPictures/nocrop"
+ratio = 1.345
+def crop_image_top_ratio(image):
+    width, height = image.size
+    newHeight = int(ratio * width)
+    image = image.crop([0,height - newHeight,width,height])
+    return image 
+
+
+img_path = "/Users/anthonyluo/Documents/WebsiteFullPictures/square"
 output_path = "images/food"
-onlyfiles = [f for f in listdir(img_path) if isfile(join(img_path, f))]
-dimension = 1000
+files = [f for f in listdir(img_path) if isfile(join(img_path, f))]
+dimension = 800
 
-crop = False 
+cropFunction = crop_image_square
 
-print(onlyfiles)
-for f in onlyfiles:
+print(files)
+for f in files:
+    if(".DS_Store" in f):
+        continue
     image = Image.open(join(img_path, f))
-    if(crop):
-        image = crop_image(image)
+    image = cropFunction(image)
     width, height = image.size
     image = image.resize((dimension,int(dimension * (height/width))))
     image.save(f'{output_path}/{f}', optimize=True, quality=90)  
